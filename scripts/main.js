6c07be1ab97d17cc8d56d328ec75e154fdc0920c
antonisoaho <isoahoanton@gmail.com>
2023-10-10 19:29:53 +0200

    changes - another js-file

--- scripts/main.js
+++ scripts/main.js
@@ -64,36 +64,3 @@ screenWidthChange(mediaQueryLtl, "Ltl-width");
 mediaQueryMin.addListener(() => screenWidthChange(mediaQueryMin, "Min-width"));
 mediaQueryMax.addListener(() => screenWidthChange(mediaQueryMax, "Max-width"));
 mediaQueryLtl.addListener(() => screenWidthChange(mediaQueryLtl, "Ltl-width"));
-
-// EventListeners for AI-companion
-
-const slider = document.getElementById("priceSlider");
-const maxPriceDisplay = document.getElementById("sliderValue");
-const roomCards = document.querySelectorAll(".section__figure.room");
-const specialCheckbox = document.getElementById("specialCheckbox");
-const items = document.querySelectorAll(".room");
-
-function updateItemVisibility() {
-  const maxPrice = parseInt(slider.value);
-  const showSpecial = specialCheckbox.checked;
-
-  roomCards.forEach((card) => {
-    const cardPrice = parseInt(card.getAttribute("data-price"));
-    const meetsPrice = cardPrice <= maxPrice;
-    const meetsSpecial = showSpecial
-      ? card.classList.contains("special")
-      : true;
-    card.style.display = meetsPrice && meetsSpecial ? "inline-block" : "none";
-  });
-}
-
-updateItemVisibility();
-
-slider.addEventListener("input", () => {
-  maxPriceDisplay.innerHTML = slider.value;
-  updateItemVisibility();
-});
-
-specialCheckbox.addEventListener("change", () => {
-  updateItemVisibility();
-});
